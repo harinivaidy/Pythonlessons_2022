90bb327c9f54a6adecda762b5fd77ed849ff3efd
Harini Vaidyanathan <harinivaidy2001@gmail.com>
2022-11-14 06:54:48 -0800

    commit test1.py

---
+++ test1.py
@@ -0,0 +1,8 @@
+for i in range(100):
+    print(i)
+print("hello")
+print("Christian grey")
+print("Pitbulll")
+print("modifying now")
+print("Wedding day for Jessica")
+print("I will be going in by myself")
